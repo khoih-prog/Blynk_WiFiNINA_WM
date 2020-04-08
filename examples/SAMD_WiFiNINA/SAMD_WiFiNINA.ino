8d989079b3a7c8f1e052ba79dbc15c8425792042
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-04-08 02:15:14 -0400

    v1.0.0
    ### New Releases v1.0.0
    
    1. This release of very-easy-to-use will help you to eliminate hardcoding your Wifi and Blynk credentials for Mega/Teensy boards running WiFiNINA shields, and updating/reflashing every time when you need to change them.
    2. Configuration data are stored in configurable location in EEPROM.
    3. When WiFi and/or Blynk connection is lost, the WM will try auto-reconnect.
    4. `Config Portal Static IP address, Name and Password.`
    5. `Static IP address, Gateway, Subnet Mask.`
    6. Currently support AVR Mega and SAMD (ZERO, MKR, NANO_33_IOT, M0, M0 Pro, AdaFruit CIRCUITPLAYGROUND_EXPRESS, etc.) boards
    7. Enhance GUI.

---
+++ examples/SAMD_WiFiNINA/SAMD_WiFiNINA.ino
@@ -0,0 +1,252 @@
+/****************************************************************************************************************************
+   SAMD_WiFiNINA.ino
+   For SAMD using WiFiNINA Shield/Module
+
+   Blynk_WiFiNINA_WM is a library for the Mega, Teensy, SAM DUE and SAMD boards (https://github.com/khoih-prog/Blynk_WiFiNINA_WM)
+   to enable easy configuration/reconfiguration and autoconnect/autoreconnect of WiFiNINA/Blynk
+
+   Modified from Blynk library v0.6.1 https://github.com/blynkkk/blynk-library/releases
+   Built by Khoi Hoang https://github.com/khoih-prog/Blynk_WM
+   Licensed under MIT license
+   Version: 1.0.0
+
+   Original Blynk Library author:
+   @file       BlynkSimpleWiFiNINA.h
+   @author     Volodymyr Shymanskyy
+   @license    This project is released under the MIT License (MIT)
+   @copyright  Copyright (c) 2018 Volodymyr Shymanskyy
+   @date       Sep 2018
+   @brief
+
+   Version Modified By   Date        Comments
+   ------- -----------  ----------   -----------
+    1.0.0   K Hoang      07/04/2020  Initial coding
+ *****************************************************************************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define DEBUG_WIFI_WEBSERVER_PORT Serial
+#define WIFININA_DEBUG_OUTPUT     Serial
+#define BLYNK_PRINT Serial
+
+#if    ( defined(ARDUINO_SAMD_ZERO) || defined(ARDUINO_SAMD_MKR1000) || defined(ARDUINO_SAMD_MKRWIFI1010) \
+      || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_SAMD_MKRFox1200) || defined(ARDUINO_SAMD_MKRWAN1300) || defined(ARDUINO_SAMD_MKRWAN1310) \
+      || defined(ARDUINO_SAMD_MKRGSM1400) || defined(ARDUINO_SAMD_MKRNB1500) || defined(ARDUINO_SAMD_MKRVIDOR4000) || defined(__SAMD21G18A__) \
+      || defined(ARDUINO_SAMD_CIRCUITPLAYGROUND_EXPRESS) )
+#if defined(WIFININA_USE_SAMD)
+#undef WIFININA_USE_SAMD
+#undef WIFI_USE_SAMD
+#endif
+#define WIFININA_USE_SAMD      true
+#define WIFI_USE_SAMD          true
+#else
+#error This code is intended to run only on the SAMD boards ! Please check your Tools->Board setting.
+#endif
+
+#if defined(WIFININA_USE_SAMD)
+
+#if defined(ARDUINO_SAMD_ZERO)
+#define BOARD_TYPE      "SAMD Zero"
+#elif defined(ARDUINO_SAMD_MKR1000)
+#define BOARD_TYPE      "SAMD MKR1000"
+#elif defined(ARDUINO_SAMD_MKRWIFI1010)
+#define BOARD_TYPE      "SAMD MKRWIFI1010"
+#elif defined(ARDUINO_SAMD_NANO_33_IOT)
+#define BOARD_TYPE      "SAMD NANO_33_IOT"
+#elif defined(ARDUINO_SAMD_MKRFox1200)
+#define BOARD_TYPE      "SAMD MKRFox1200"
+#elif ( defined(ARDUINO_SAMD_MKRWAN1300) || defined(ARDUINO_SAMD_MKRWAN1310) )
+#define BOARD_TYPE      "SAMD MKRWAN13X0"
+#elif defined(ARDUINO_SAMD_MKRGSM1400)
+#define BOARD_TYPE      "SAMD MKRGSM1400"
+#elif defined(ARDUINO_SAMD_MKRNB1500)
+#define BOARD_TYPE      "SAMD MKRNB1500"
+#elif defined(ARDUINO_SAMD_MKRVIDOR4000)
+#define BOARD_TYPE      "SAMD MKRVIDOR4000"
+#elif defined(ARDUINO_SAMD_CIRCUITPLAYGROUND_EXPRESS)
+#define BOARD_TYPE      "SAMD ARDUINO_SAMD_CIRCUITPLAYGROUND_EXPRESS"
+#else
+#define BOARD_TYPE      "SAMD Unknown"
+#endif
+#endif
+
+// Start location in EEPROM to store config data. Default 0
+// Config data Size currently is 128 bytes)
+#define EEPROM_START     0
+
+#define USE_BLYNK_WM      true
+//#define USE_BLYNK_WM      false
+
+#if USE_BLYNK_WM
+#include <BlynkSimpleWiFiNINA_SAMD_WM.h>
+
+/////////////// Start dynamic Credentials ///////////////
+
+//Defined in <BlynkSimpleEsp32_GSM_WFM.h>
+/**************************************
+  #define MAX_ID_LEN                5
+  #define MAX_DISPLAY_NAME_LEN      16
+
+  typedef struct
+  {
+  char id             [MAX_ID_LEN + 1];
+  char displayName    [MAX_DISPLAY_NAME_LEN + 1];
+  char *pdata;
+  uint8_t maxlen;
+  } MenuItem;
+**************************************/
+
+#define MAX_MQTT_SERVER_LEN      34
+char MQTT_Server  [MAX_MQTT_SERVER_LEN]   = "";
+
+#define MAX_MQTT_PORT_LEN        6
+char MQTT_Port   [MAX_MQTT_PORT_LEN]  = "";
+
+#define MAX_MQTT_USERNAME_LEN      34
+char MQTT_UserName  [MAX_MQTT_USERNAME_LEN]   = "";
+
+#define MAX_MQTT_PW_LEN        34
+char MQTT_PW   [MAX_MQTT_PW_LEN]  = "";
+
+#define MAX_MQTT_SUBS_TOPIC_LEN      34
+char MQTT_SubsTopic  [MAX_MQTT_SUBS_TOPIC_LEN]   = "";
+
+#define MAX_MQTT_PUB_TOPIC_LEN       34
+char MQTT_PubTopic   [MAX_MQTT_PUB_TOPIC_LEN]  = "";
+
+MenuItem myMenuItems [] =
+{
+  { "mqtt", "MQTT Server",      MQTT_Server,      MAX_MQTT_SERVER_LEN },
+  { "mqpt", "Port",             MQTT_Port,        MAX_MQTT_PORT_LEN   },
+  { "user", "MQTT UserName",    MQTT_UserName,    MAX_MQTT_USERNAME_LEN },
+  { "mqpw", "MQTT PWD",         MQTT_PW,          MAX_MQTT_PW_LEN },
+  { "subs", "Subs Topics",      MQTT_SubsTopic,   MAX_MQTT_SUBS_TOPIC_LEN },
+  { "pubs", "Pubs Topics",      MQTT_PubTopic,    MAX_MQTT_PUB_TOPIC_LEN },
+};
+
+uint16_t NUM_MENU_ITEMS = sizeof(myMenuItems) / sizeof(MenuItem);  //MenuItemSize;
+/////// // End dynamic Credentials ///////////
+
+#else
+#include <BlynkSimpleWiFiNINA_SAMD.h>
+
+#define USE_LOCAL_SERVER      true
+
+#if USE_LOCAL_SERVER
+char auth[] = "****";
+String BlynkServer = "account.duckdns.org";
+
+//String BlynkServer = "192.168.2.112";
+#else
+char auth[] = "****";
+String BlynkServer = "blynk-cloud.com";
+#endif
+
+#define BLYNK_SERVER_HARDWARE_PORT    8080
+
+// Your WiFi credentials.
+//char ssid[] = "****";
+//char pass[] = "****";
+
+char ssid[] = "HueNet1";
+char pass[] = "jenniqqs";
+
+#endif
+
+// Your Megay <-> ESP8266 baud rate:
+#define ESP8266_BAUD 115200
+
+void heartBeatPrint(void)
+{
+  static int num = 1;
+
+  if (Blynk.connected())
+  {
+    Serial.print("B");
+  }
+  else
+  {
+    Serial.print("F");
+  }
+
+  if (num == 80)
+  {
+    Serial.println();
+    num = 1;
+  }
+  else if (num++ % 10 == 0)
+  {
+    Serial.print(" ");
+  }
+}
+
+void check_status()
+{
+  static unsigned long checkstatus_timeout = 0;
+
+#define STATUS_CHECK_INTERVAL     15000L
+
+  // Send status report every STATUS_REPORT_INTERVAL (60) seconds: we don't need to send updates frequently if there is no status change.
+  if ((millis() > checkstatus_timeout) || (checkstatus_timeout == 0))
+  {
+    // report status to Blynk
+    heartBeatPrint();
+
+    checkstatus_timeout = millis() + STATUS_CHECK_INTERVAL;
+  }
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+  while (!Serial);
+  //delay(1000);
+
+  Serial.println("\nStart Blynk_WiFiNINA_WM using WiFiNINA_Shield on " + String(BOARD_TYPE));
+
+#if USE_BLYNK_WM
+  Serial.println(F("Start Blynk_WM"));
+  Blynk.setConfigPortalIP(IPAddress(192, 168, 120, 1));
+  //Blynk.setConfigPortal("SAMD", "MySAMD");
+  Blynk.begin("SAMD-WiFiNINA");
+#else
+  Serial.println(F("Start Blynk"));
+  Blynk.begin(auth, ssid, pass, BlynkServer.c_str(), BLYNK_SERVER_HARDWARE_PORT);
+#endif
+}
+
+void displayCredentials(void)
+{
+  Serial.println("Your stored Credentials :");
+
+  for (int i = 0; i < NUM_MENU_ITEMS; i++)
+  {
+    Serial.println(String(myMenuItems[i].displayName) + " = " + myMenuItems[i].pdata);
+  }
+}
+
+void loop()
+{
+  Blynk.run();
+
+  static bool displayedCredentials = false;
+
+  if (!displayedCredentials)
+  {
+    for (int i = 0; i < NUM_MENU_ITEMS; i++)
+    {
+      if (!strlen(myMenuItems[i].pdata))
+      {
+        break;
+      }
+
+      if ( i == (NUM_MENU_ITEMS - 1) )
+      {
+        displayedCredentials = true;
+        displayCredentials();
+      }
+    }
+  }
+    
+  check_status();
+}
